32a75a54fa849ac3a0a56f672893f09e2e4d3dd8
songlinyang <614580167@qq.com>
2019-02-26 16:37:53 +0800

    引入未引入的类

--- code/note/src/main/java/com/ysl/note/db/DbManager.java
+++ code/note/src/main/java/com/ysl/note/db/DbManager.java
@@ -2,10 +2,7 @@ package com.ysl.note.db;
 
 import java.io.Serializable;
 import java.lang.reflect.Method;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
